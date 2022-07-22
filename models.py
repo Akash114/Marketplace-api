79e4fbdb0415eb954df0724802efceeab5e84fd5
Akash <akash.lamprostech@gmail.com>
2022-07-22 15:40:26 +0530

    From field value changed

--- models.py
+++ models.py
@@ -41,6 +41,6 @@ class Transactions(Document):
     asset_id = IntField(min_value=1)
     event = StringField(required=True, max_length=50)
     to = StringField(required=True, max_length=100)
-    From = StringField(required=True, max_length=100)
+    From = StringField(required=True, max_length=500)
     price = StringField(required=True, max_length=100)
     timestamp = DateTimeField(default=datetime.datetime.now)
